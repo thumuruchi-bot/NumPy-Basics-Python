51452f3fdfbf784fc46550c6e779409b4f41e500
T.Ruchitha <thumuruchi@gmail.com>
2026-09-20 11:18:14 +0530

    Add basic NumPy operations example

---
+++ numpy_basics.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([4, 5, 6])
+
+print("Array 1:", arr1)
+print("Array 2:", arr2)
+
+print("Addition:", arr1 + arr2)
+print("Multiplication:", arr1 * arr2)
